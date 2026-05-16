9e4d6a29de429d9a931da664798cc1744b39971b
antonios-azer <antoniosazer@gmail.com>
2026-05-16 19:54:34 +0300

    task_4.dart

--- antonios-azer-tasks-main/antonios-azer-tasks-main/antonios-azer-read-main/task_4.dart
+++ antonios-azer-tasks-main/antonios-azer-tasks-main/antonios-azer-read-main/task_4.dart
@@ -1,15 +1,11 @@
-// Step 1: Create Person class
 class Person {
   String name;
-  int _age; // private age variable
+  int _age;
 
-  // Constructor
   Person(this.name, this._age);
 
-  // Getter for age
   int get age => _age;
 
-  // Setter for age
   set age(int value) {
     if (value > 0) {
       _age = value;
@@ -18,18 +14,15 @@ class Person {
     }
   }
 
-  // Static variable
-  static String universityName = "Sohag University";
+  static String universityName = "sohag University";
 }
 
-// Step 2: Create Employee class extending Person
 class Employee extends Person {
   double salary;
 
-  // Constructor using super
-  Employee(String name, int age, this.salary) : super(name, age);
+  Employee(String name, int age, this.salary)
+      : super(name, age);
 
-  // Function to show info
   void showInfo() {
     print("University: ${Person.universityName}");
     print("Name: $name");
@@ -38,15 +31,15 @@ class Employee extends Person {
   }
 }
 
-// Step 3: Abstract class Skills
 abstract class Skills {
   void programming();
+
   void communication();
 }
 
-// Step 4: Developer class
 class Developer extends Employee implements Skills {
-  Developer(String name, int age, double salary) : super(name, age, salary);
+  Developer(String name, int age, double salary)
+      : super(name, age, salary);
 
   @override
   void programming() {
@@ -59,9 +52,13 @@ class Developer extends Employee implements Skills {
   }
 }
 
-// Step 5: Main function
 void main() {
-  Developer dev = Developer("Ahmed", 25, 8000);
+  Developer dev = Developer(
+    "Antonios Azer",
+    25,
+    8000,
+  );
+
   dev.showInfo();
   dev.programming();
   dev.communication();
